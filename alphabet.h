6d2e533a147ded60ab9bd021ad47c09579b98e3c
Yeuph <euphoric.daniel@gmail.com>
2023-01-29 13:21:44 -0500

    Updated to correct file.

--- alphabet.h
+++ alphabet.h
@@ -1,32 +1,37 @@
+#pragma once
+
 #include <bitset>
 
-#include "alphabet.h"
+//10 bits for encoding alphabet sets
+const size_t ALPHABET_ENCODING_SIZE_ = 10;
+//ABC
+const size_t ALPHABET_SETS_CNT_ = 3;
+//Size of data being analyzed
+const size_t COMPRESSION_CHUNK_SIZE_ = 8;
 
 
-Alphabet ChooseAlphabet(const char* in, const size_t in_size) {
+struct Set {
     // TODO
-    Set sets[ALPHABET_SETS_CNT_];
-    return Alphabet(sets);
-}
+};
 
-Alphabet::Alphabet(const std::bitset<ALPHABET_ENCODING_SIZE_>& bit_encoding) {
-    // TODO
-}
 
-std::bitset<ALPHABET_ENCODING_SIZE_> Alphabet::ToBitset() const {
-    std::bitset<ALPHABET_ENCODING_SIZE_> encoded;
-    // TODO
-    return encoded;
-}
+class Alphabet {
+public:
+    Alphabet(const Set encoding_sets[ALPHABET_SETS_CNT_]): encoding_sets_(encoding_sets_) {}
 
-size_t Alphabet::Encode(const char* in, size_t in_size, char* out) const {
-    size_t out_size;
-    // TODO
-    return out_size;
-}
+    //Method to tell compiler which 10 bit instruction to read
+    Alphabet(const std::bitset<ALPHABET_ENCODING_SIZE_>& bit_encoding);
 
-size_t Alphabet::Decode(const char* in, size_t in_size, char* out) const {
-    size_t out_size;
-    // TODO
-    return out_size;
-}
+    //Dump to bit representation
+    std::bitset<ALPHABET_ENCODING_SIZE_> ToBitset() const;
+
+    size_t Encode(const char* in, size_t in_size, char* out) const;
+
+    size_t Decode(const char* in, size_t in_size, char* out) const;
+
+private:
+    Set encoding_sets_[ALPHABET_SETS_CNT_];
+};
+
+
+Alphabet ChooseAlphabet(const char* in, const size_t in_size);
